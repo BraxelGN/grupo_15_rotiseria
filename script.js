38827bf0c989a788f47ce989fa8e9599bfafa9ce
BraxelGN <patu_game@hotmail.com>
2023-11-02 16:41:03 -0300

    Agregada satisfactoriamente la pagina de formulario y sus estilos

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ window.addEventListener('load', function() {
     }, 500); // Ajusta la duración de la transición aquí
 });
 
+// Formulario
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contact-form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        if (name === '' || email === '' || message === '') {
+            alert('Por favor, complete todos los campos.');
+        } else {
+            alert('Formulario enviado con éxito.');
+            form.reset();
+        }
+    });
+});
+
